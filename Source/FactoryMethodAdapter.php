e34e0a7dd12a06e835ba8ce8eef9bd87b0807c9b
AmyStephen <AmyStephen@gmail.com>
2014-06-08 10:26:16 -0500

    FactoryMethodBase

---
+++ Source/FactoryMethodAdapter.php
@@ -0,0 +1,321 @@
+<?php
+/**
+ * Factory Method Adapter
+ *
+ * @package    Molajo
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ * @copyright  2014 Amy Stephen. All rights reserved.
+ */
+namespace Molajo\IoC;
+
+use stdClass;
+
+/**
+ * Factory Method Adapter
+ *
+ * FactoryMethodAdapter - FactoryMethodInstantiate - FactoryMethodBase
+ *
+ * @author     Amy Stephen
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ * @copyright  2014 Amy Stephen. All rights reserved.
+ * @since      1.0.0
+ */
+abstract class FactoryMethodAdapter extends FactoryMethodInstantiate
+{
+    /**
+     * IoC ID from Controller
+     *
+     * @var    string
+     * @since  1.0.0
+     */
+    protected $ioc_id = null;
+
+    /**
+     * Product Name
+     *
+     * @var    string
+     * @since  1.0.0
+     */
+    protected $product_name = null;
+
+    /**
+     * Product Namespace
+     *
+     * @var    string
+     * @since  1.0.0
+     */
+    protected $product_namespace = null;
+
+    /**
+     * Static Instance Indicator
+     *
+     * @var    boolean
+     * @since  1.0.0
+     * @static
+     */
+    protected $static_instance_indicator = false;
+
+    /**
+     * Store Instance Indicator
+     *
+     * @var    boolean
+     * @since  1.0.0
+     */
+    protected $store_instance_indicator = false;
+
+    /**
+     * Store Properties
+     *
+     * @var    boolean
+     * @since  1.0.0
+     */
+    protected $store_properties_indicator = false;
+
+    /**
+     * Options
+     *
+     * @var    array
+     * @since  1.0.0
+     */
+    protected $options = array();
+
+    /**
+     * Reflection Parameters
+     *
+     * @var    array
+     * @since  1.0.0
+     */
+    protected $reflection = array();
+
+    /**
+     * Dependencies
+     *
+     * @var    array
+     * @since  1.0.0
+     */
+    protected $dependencies = array();
+
+    /**
+     * Product Result
+     *
+     * @var    object
+     * @since  1.0.0
+     */
+    protected $product_result = null;
+
+    /**
+     * Static Product Result
+     *
+     * @static
+     * @var    object  Services
+     * @since  1.0.0
+     */
+    protected static $static_product_result = null;
+
+    /**
+     * Container Entries to remove from IoCC
+     *
+     * @var    array
+     * @since  1.0.0
+     */
+    protected $remove_container_entries = array();
+
+    /**
+     * Services to update within the IoCC
+     *
+     * @var    array
+     * @since  1.0.0
+     */
+    protected $set_container_entries = array();
+
+    /**
+     * Services to schedule for Product Creation
+     *
+     * @var    array
+     * @since  1.0.0
+     */
+    protected $schedule_factory_methods = array();
+
+    /**
+     * List of Property Array
+     *
+     * @var    array
+     * @since  1.0.0
+     */
+    protected $factory_method_adapter_property_array
+        = array(
+            'ioc_id',
+            'product_name',
+            'product_namespace',
+            'static_instance_indicator',
+            'store_instance_indicator',
+            'store_properties_indicator',
+            'reflection',
+            'dependencies',
+            'product_result',
+            'static_product_result',
+            'remove_container_entries',
+            'set_container_entries',
+            'schedule_factory_methods'
+        );
+
+    /**
+     * Constructor
+     *
+     * @param   $options
+     *
+     * @since  1.0.0
+     */
+    public function __construct(array $options = array())
+    {
+        $this->schedule_factory_methods = array();
+
+        if (is_array($options)) {
+        } else {
+            $options = array();
+        }
+
+        if (count($options) > 0) {
+            foreach ($this->factory_method_adapter_property_array as $property) {
+                if (isset($options[$property])) {
+                    $this->$property = $options[$property];
+                    unset($options[$property]);
+                }
+            }
+        }
+
+        $this->options = $options;
+    }
+
+    /**
+     * Factory Method Controller requests Product Namespace from Factory Method
+     *
+     * @return  string
+     * @since  1.0.0
+     */
+    public function getNamespace()
+    {
+        return $this->product_namespace;
+    }
+
+    /**
+     * Factory Method Controller requests Service Options from Factory Method
+     *
+     * @return  array
+     * @since  1.0.0
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * Factory Method Controller retrieves "store instance indicator" from Factory Method
+     *
+     * @return  boolean
+     * @since  1.0.0
+     */
+    public function getStoreContainerEntryIndicator()
+    {
+        return $this->store_instance_indicator;
+    }
+
+    /**
+     * Read File
+     *
+     * @param   string $file_name
+     *
+     * @return  array
+     * @since   1.0.0
+     */
+    protected function readFile($file_name)
+    {
+        if (file_exists($file_name)) {
+        } else {
+            return array();
+        }
+
+        $input = file_get_contents($file_name);
+
+        return $this->readFileIntoArray($input);
+    }
+
+    /**
+     * Process JSON string by loading into an array
+     *
+     * @param   string $input
+     *
+     * @return  array
+     * @since   1.0.0
+     */
+    protected function readFileIntoArray($input)
+    {
+        $temp = json_decode($input);
+
+        $temp_array = array();
+        if (count($temp) > 0) {
+            $temp_array = array();
+            foreach ($temp as $key => $value) {
+                $temp_array[$key] = $value;
+            }
+        }
+
+        return $temp_array;
+    }
+
+    /**
+     * Sort Object
+     *
+     * @param   object $input_object
+     *
+     * @return  stdClass
+     * @since  1.0.0
+     */
+    protected function sortObject($input_object)
+    {
+        $hold_array = $this->sortObjectLoadIntoArray($input_object);
+
+        return $this->sortObjectLoadSortedArrayIntoObject($hold_array);
+    }
+
+    /**
+     * Sort Object
+     *
+     * @param   object $input_object
+     *
+     * @return  array
+     * @since   1.0.0
+     */
+    protected function sortObjectLoadIntoArray($input_object)
+    {
+        $hold_array = array();
+        foreach (\get_object_vars($input_object) as $key => $value) {
+            $hold_array[$key] = $value;
+        }
+
+        ksort($hold_array);
+
+        return $hold_array;
+    }
+
+    /**
+     * Sort Object
+     *
+     * @param   array $hold_array
+     *
+     * @return  stdClass
+     * @since   1.0.0
+     */
+    protected function sortObjectLoadSortedArrayIntoObject(array $hold_array = array())
+    {
+        $new_object = new stdClass();
+
+        if (count($hold_array) > 0) {
+            foreach ($hold_array as $key => $value) {
+                $new_object->$key = $value;
+            }
+        }
+
+        return $new_object;
+    }
+}
---
+++ Source/FactoryMethodInstantiate.php
@@ -0,0 +1,279 @@
+<?php
+/**
+ * Factory Method Instantiate
+ *
+ * @package    Molajo
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ * @copyright  2014 Amy Stephen. All rights reserved.
+ */
+namespace Molajo\IoC;
+
+use CommonApi\Exception\RuntimeException;
+use Exception;
+use ReflectionClass;
+
+/**
+ * Factory Method Instantiate
+ *
+ * FactoryMethodAdapter - FactoryMethodInstantiate - FactoryMethodBase
+ *
+ * @author     Amy Stephen
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ * @copyright  2014 Amy Stephen. All rights reserved.
+ * @since      1.0.0
+ */
+abstract class FactoryMethodInstantiate extends FactoryMethodAdapter
+{
+    /**
+     * Service instantiated automatically or within this method by the Factory Method
+     *
+     * @return  $this
+     * @since   1.0.0
+     */
+    public function instantiateClass()
+    {
+        if ($this->product_namespace === null) {
+            return $this;
+        }
+
+        if ($this->static_instance_indicator === true) {
+            self::instantiateStatic($this->product_namespace);
+            return $this;
+        }
+
+        $dependencies = array();
+
+        if (count($this->reflection) > 0) {
+            $dependencies = $this->processReflectionDependencies($dependencies);
+        }
+
+        return $this->instantiateClassNotStatic($dependencies);
+    }
+
+    /**
+     * Process Reflection Dependencies
+     *
+     * @param   array $dependencies
+     *
+     * @return  mixed
+     * @since   1.0.0
+     */
+    protected function processReflectionDependencies(array $dependencies = array())
+    {
+        foreach ($this->reflection as $dependency) {
+
+            $dependency_name  = $dependency->name;
+            $dependency_value = $dependency->default_value;
+
+            foreach ($this->dependencies as $key => $value) {
+
+                if (strtolower($dependency_name) == strtolower($key)) {
+                    $dependency_value = $value;
+                    break;
+                }
+            }
+
+            $dependencies[$dependency_name] = $dependency_value;
+        }
+
+        return $dependencies;
+    }
+
+    /**
+     * Instantiate Class with dependencies
+     *
+     * @param   array $dependencies
+     *
+     * @return  FactoryMethodBase
+     * @since   1.0.0
+     * @throws  \CommonApi\Exception\RuntimeException
+     */
+    protected function instantiateClassNotStaticTryCatch(array $dependencies = array())
+    {
+        try {
+            return $this->instantiateClassNotStatic($dependencies);
+
+        } catch (Exception $e) {
+
+            throw new RuntimeException(
+                'IoC instantiateClass with dependencies Failed: '
+                . $this->product_namespace . ' ' . $e->getMessage()
+            );
+        }
+    }
+
+    /**
+     * Instantiate Class with dependencies
+     *
+     * @param   array $dependencies
+     *
+     * @return  FactoryMethodBase
+     * @since   1.0.0
+     * @throws  \CommonApi\Exception\RuntimeException
+     */
+    protected function instantiateClassNotStatic(array $dependencies = array())
+    {
+        if (method_exists($this->product_namespace, '__construct') && count($dependencies) > 0) {
+            $reflection           = new ReflectionClass($this->product_namespace);
+            $this->product_result = $reflection->newInstanceArgs($dependencies);
+
+        } else {
+            $class                = $this->product_namespace;
+            $this->product_result = new $class();
+        }
+
+        return $this;
+    }
+
+    /**
+     * Instantiate Service Class Statically
+     *
+     * @param   string $product_namespace
+     *
+     * @static
+     *
+     * @return  void
+     * @since   1.0.0
+     * @throws  \CommonApi\Exception\RuntimeException
+     */
+    public static function instantiateStatic($product_namespace)
+    {
+        try {
+            self::$static_product_result = new $product_namespace();
+
+        } catch (Exception $e) {
+            throw new RuntimeException(
+                'IoC instantiateClass instantiateStatic  ' . $product_namespace . ' ' . $e->getMessage()
+            );
+        }
+
+        return;
+    }
+
+    /**
+     * Logic contained within this method is invoked after the class construction
+     *  and can be used for setter logic or other post-construction processing
+     *
+     * @return  $this
+     * @since   1.0.0
+     */
+    public function onAfterInstantiation()
+    {
+        return $this;
+    }
+
+    /**
+     * Factory Method Controller requests Product Result for just created Class
+     *
+     * @return  object
+     * @since   1.0.0
+     */
+    public function getProductValue()
+    {
+        if ($this->static_instance_indicator === true) {
+            return $this->getProductValueStatic();
+        }
+
+        if ($this->store_instance_indicator === true) {
+            return $this->getProductValueInstance();
+        }
+
+        if ($this->store_properties_indicator === true) {
+            return $this->getProductValueProperties();
+        }
+
+        return $this->getProductValueDoNotSave();
+    }
+
+    /**
+     * Get Product Value: Static
+     *
+     * @return  object
+     * @since   1.0.0
+     */
+    protected function getProductValueStatic()
+    {
+        $this->store_instance_indicator   = true;
+        $this->store_properties_indicator = false;
+
+        return self::$static_product_result;
+    }
+
+    /**
+     * Get Product Value: Instance
+     *
+     * @return  object
+     * @since   1.0.0
+     */
+    protected function getProductValueInstance()
+    {
+        $this->store_properties_indicator = false;
+        $this->static_instance_indicator  = false;
+
+        return $this->product_result;
+    }
+
+    /**
+     * Get Product Value: Properties
+     *
+     * @return  object
+     * @since   1.0.0
+     */
+    protected function getProductValueProperties()
+    {
+
+        $this->store_instance_indicator  = true;
+        $this->static_instance_indicator = false;
+
+        return $this->product_result->get('*', array());
+    }
+
+    /**
+     * Get Product Value: Properties
+     *
+     * @return  object
+     * @since  1.0.0
+     */
+    protected function getProductValueDoNotSave()
+    {
+
+        $this->store_instance_indicator   = false;
+        $this->store_properties_indicator = false;
+        $this->static_instance_indicator  = false;
+
+        return $this->product_result;
+    }
+
+    /**
+     * Request for array of Products to be removed from the IoC Container
+     *
+     * @return  array
+     * @since  1.0.0
+     */
+    public function removeContainerEntries()
+    {
+        return $this->remove_container_entries;
+    }
+
+    /**
+     * Request for array of Products and Values to be saved to the IoC Container
+     *
+     * @return  array
+     * @since  1.0.0
+     */
+    public function setContainerEntries()
+    {
+        return $this->set_container_entries;
+    }
+
+    /**
+     * Request for array of Factory Methods to be Scheduled
+     *
+     * @return  array
+     * @since  1.0.0
+     */
+    public function scheduleFactories()
+    {
+        return $this->schedule_factory_methods;
+    }
+}
